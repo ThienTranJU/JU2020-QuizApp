97a2127a6965d7d30b5a326de25057a0fd7f9c04
Quang Thien Tran <trqu19ba@student.ju.se>
2020-11-07 16:39:49 +0100

    Added custom launch screen Added randomisation of quiz questions Added a background image to result view Added an app icon

--- QuizApp_LabProject/ResultView.swift
+++ QuizApp_LabProject/ResultView.swift
@@ -11,6 +11,8 @@ class ResultView: UIView {
     
     let titleLabel = UILabel()
     let resultLabel = UILabel()
+    let backgroundImage = UIImage(named: "backgroundImage")
+    let backgroundImageView = UIImageView()
     let button = UIButton()
     
     override init(frame: CGRect) {
@@ -24,32 +26,37 @@ class ResultView: UIView {
     }
 
     private func setupView() {
-        backgroundColor = .white
-        titleLabel.textColor = .black
+        backgroundImageView.image = backgroundImage
+        addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        backgroundImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        titleLabel.textColor = .white
         titleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
         titleLabel.adjustsFontForContentSizeCategory = true
         titleLabel.text = NSLocalizedString("Your Result", comment: "Result title")
         addSubview(titleLabel)
-        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         
+        resultLabel.textColor = .white
         resultLabel.font = UIFont.preferredFont(forTextStyle: .callout)
         resultLabel.adjustsFontForContentSizeCategory = true
         addSubview(resultLabel)
-        
         resultLabel.translatesAutoresizingMaskIntoConstraints = false
         resultLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
         resultLabel.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor).isActive = true
         
         
-        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
         button.setTitle(NSLocalizedString("Back to menu", comment: "Result button title"), for: .normal)
         addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 20).isActive = true
         button.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
     }
     
 }
--- QuizApp_LabProject/StartViewController.swift
+++ QuizApp_LabProject/StartViewController.swift
@@ -22,8 +22,12 @@ class StartViewController: UIViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let questionViewController = segue.destination as? QuestionViewController {
-            let question = Question(category: "Food", type: .multiple, difficulty: .easy, question: "What is my favourite food?", correctAnswer: "🍕", inccorectAnswer: ["🐟","🍚","🥩"])
-            questionViewController.question = question
+            let questionVariation1 = Question(category: "Food", type: .multiple, difficulty: .easy, question: "What is my favourite food?", correctAnswer: "🍕", inccorectAnswer: ["🐟","🍚","🥩"])
+            let questionVariation2 = Question(category: "Drink", type: .multiple, difficulty: .medium, question: "What is my favourite drink?", correctAnswer: "🍻", inccorectAnswer: ["🧉","🍷","☕️"])
+            let questionVariation3 = Question(category: "Emotion", type: .multiple, difficulty: .easy, question: "What is my favourite emotion", correctAnswer: "🤩", inccorectAnswer: ["🥳","🤪","🤨"])
+            var questions = [questionVariation1,questionVariation2,questionVariation3]
+            questions.shuffle()
+            questionViewController.question = questions.removeFirst()
         }
     }
 
